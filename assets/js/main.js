dff75067d3baf7d50b68b93caab91f9d48669d9c
unknown <Mukesh0702>
2019-12-16 20:20:45 +0545

    disable zoom

--- assets/js/main.js
+++ assets/js/main.js
@@ -274,5 +274,7 @@ document.addEventListener('gestureend', function(e) {
   document.body.style.zoom = 0.99;
 });
 document.addEventListener('touchmove', function (event) {
-  if (event.scale !== 1) { event.preventDefault(); }
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  document.body.style.zoom = 0.99;
 }, false);
