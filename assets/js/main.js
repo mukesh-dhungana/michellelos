21ecd3303038714c8150cf5a414467d3ce002edb
unknown <Mukesh0702>
2019-12-17 19:59:29 +0545

    fix zoom safari

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,23 +256,20 @@ window.onload = function () {
   //$(".services-center-fit").css("height", document.getElementById('serviceSection').scrollHeight);
 };
 
-// document.addEventListener('gesturestart', function(e) {
-//   e.preventDefault();
-//   // special hack to prevent zoom-to-tabs gesture in safari
-//   document.body.style.zoom = 0.99;
-// });
+document.addEventListener('gesturestart', function (e) {
+  e.preventDefault();
+  document.body.style.zoom = 0.99;
+});
 
-// document.addEventListener('gesturechange', function(e) {
-//   e.preventDefault();
-//   // special hack to prevent zoom-to-tabs gesture in safari
-//   document.body.style.zoom = 0.99;
-// });
+document.addEventListener('gesturechange', function (e) {
+  e.preventDefault();
+  document.body.style.zoom = 0.99;
+});
 
-// document.addEventListener('gestureend', function(e) {
-//   e.preventDefault();
-//   // special hack to prevent zoom-to-tabs gesture in safari
-//   document.body.style.zoom = 0.99;
-// });
+document.addEventListener('gestureend', function (e) {
+  e.preventDefault();
+  document.body.style.zoom = 0.99;
+});
 
 
 
